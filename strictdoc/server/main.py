949b211340287fb40ed951b10149155bc59660e1
Stanislav Pankevich <s.pankevich@gmail.com>
2022-03-12 14:12:00 +0100

    WIP: server: basic /requirements response

--- strictdoc/server/main.py
+++ strictdoc/server/main.py
@@ -1,7 +1,83 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+import os
+from typing import List, Optional
 
-app = FastAPI()
+from fastapi import FastAPI, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from starlette.responses import JSONResponse
+
+from strictdoc.backend.sdoc.models.inline_link import InlineLink
+from strictdoc.backend.sdoc.models.requirement import Requirement
+from strictdoc.backend.sdoc.models.section import Section
+from strictdoc.backend.sdoc.reader import SDReader
+from strictdoc.core.document_iterator import DocumentCachingIterator
+from strictdoc.export.rst.rst_to_html_fragment_writer import RstToHtmlFragmentWriter
+
+
+class RequirementModel(BaseModel):
+    title: Optional[str]
+    statement: Optional[str]
+
+
+class RequirementsModel(BaseModel):
+    requirements: List[RequirementModel]
+
+
+DOC_FILE = "docs/strictdoc.sdoc"
+
+
+def create_requirements_router() -> APIRouter:
+    router = APIRouter()
+
+    @router.get("/requirements", response_class=JSONResponse)
+    def get_requirements():
+        print(os.getcwd())
+        document = SDReader().read_from_file(DOC_FILE)
+        requirements = []
+        uid = 0
+        iterator = DocumentCachingIterator(document)
+        for node in iterator.all_content():
+            uid = uid + 1
+            if isinstance(node, Requirement):
+                requirement_dict = {
+                    "type": "REQUIREMENT",
+                    "uid": uid,
+                    "title": node.title,
+                    "statement": RstToHtmlFragmentWriter.write(
+                        node.get_statement_single_or_multiline()
+                    )
+                }
+                if node.has_meta:
+                    fields = []
+                    for meta_field in node.enumerate_meta_fields():
+                        field_dict = {
+                            "uid": uid,
+                            "title": meta_field[0],
+                            "value": meta_field[1]
+                        }
+                        fields.append(field_dict)
+                    requirement_dict["fields"] = fields
+                requirements.append(requirement_dict)
+            elif isinstance(node, Section):
+                section_dict = {
+                    "type": "SECTION",
+                    "uid": uid,
+                    "title": node.title,
+                }
+                if len(node.free_texts) > 0:
+                    free_text = ""
+                    for part in node.free_texts[0].parts:
+                        if isinstance(part, str):
+                            free_text += part
+                        elif isinstance(part, InlineLink):
+                            free_text += part.link
+                    section_dict["free_text"] = RstToHtmlFragmentWriter.write(
+                        free_text
+                    )
+                requirements.append(section_dict)
+        return {"requirements": requirements}
+
+    return router
 
 
 def create_app(_):
@@ -20,6 +96,8 @@ def create_app(_):
         allow_methods=["*"],
         allow_headers=["*"],
     )
+
+    app.include_router(create_requirements_router())
 
     @app.get("/")
     def get_root():
--- tests/unit/strictdoc/server/test_server.py
+++ tests/unit/strictdoc/server/test_server.py
@@ -8,3 +8,18 @@ def test_server_01():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello": "World"}
+
+
+def test_server_02():
+    expected_json = {
+        'requirements':
+            [
+                {'statement': 'System shall do 1', 'title': 'Requirement 1'},
+                {'statement': 'System shall do 2', 'title': 'Requirement 2'},
+                {'statement': 'System shall do 3', 'title': 'Requirement 3'}
+            ]
+    }
+    client = TestClient(create_app(None))
+    response = client.get("/requirements")
+    assert response.status_code == 200
+    assert response.json() == expected_json
